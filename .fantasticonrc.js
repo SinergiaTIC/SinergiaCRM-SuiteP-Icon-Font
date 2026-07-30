33a266a9bd4a6b9147be157af935bd41b9a19257
Xavier Julian <xavier@sinergiatic.org>
2026-07-30 12:38:30 +0200

    Migrate font building to local node environment

---
+++ .fantasticonrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  inputDir: "./tmpSrc", // (required)
+  outputDir: "./suitepicon", // (required)
+  fontTypes: ["eot", "woff", "woff2", "ttf", "svg"],
+  assetTypes: ["css", "json", "json", "html"],
+  prefix: "suitepicon",
+  pathOptions: {
+    css: "./suitepicon/suitepicon-glyphs.scss",
+  },
+};
